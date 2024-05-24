a830cac1a855c68ab4c8f620af3fe5c938bd78a7
ROROROMP <9ROMP81@gmail.com>
2024-05-24 13:46:48 +0900

    0524_13

--- titanic/project/taitanic_01.py
+++ titanic/project/taitanic_01.py
@@ -101,4 +101,56 @@ plt.show()
 ・欠損値の処理をする
 ・新しいカラムを作成（特徴量エンジニアリングという）
 """
-df2 = df.drop(columns=[])
+# 特徴量として使うデータ以外をdropしてdfを作成する
+df2 = df.drop(columns=["Cabin","Fare","Ticket","SibSp","Parch","Name",])
+df2.head()
+df2.isnull().sum()
+
+print("Embarkedの欠損値: " ,df2["Embarked"].isnull().sum())
+plt.figure(figsize=(6,4))
+sns.countplot(data=df,x="Embarked")
+plt.show()
+
+# %%
+# df2のコピーを取ってから欠損値の補完をする
+# --------------------------------
+df3 = df2.copy()
+df3["Embarked"]=df3["Embarked"].fillna("S")
+print("Embarkedの欠損値: " ,df3["Embarked"].isnull().sum())
+
+# %%
+# Age欠損値の補完をする
+# --------------------------------
+print("Ageの欠損値: " ,df3["Age"].isnull().sum())
+print("Ageの最小: " ,df3["Age"].min())
+print("Ageの最大: " ,df3["Age"].max())
+
+plt.figure(figsize=(6,4))
+sns.histplot(df3["Age"],kde=False,bins=8)
+plt.show()
+# %%
+print("Ageの平均値: " ,df3["Age"].mean())
+print("Ageの中央値: " ,df3["Age"].median())
+
+# %%
+# 中央値で補完する
+age_median = df3["Age"].median()
+df3["Age"]=df3["Age"].fillna(age_median)
+print("Ageの欠損値: " ,df3["Age"].isnull().sum())
+
+# %%
+# 再度データ確認
+df3.isnull().sum()
+
+# %%
+# カテゴリ変数の数値変換
+# ワンホットエンコーディング
+ohe_embarked = pd.get_dummies(df3["Embarked"],prefix="Embarked")
+ohe_embarked.head()
+
+df4 = pd.concat([df3,ohe_embarked],axis=1)
+
+# %%
+# 不要になったEmbarkedを削除しておく
+df5 = df4.drop(columns=["Embarked"])
+df5.head()
